e7fd7e8f73398f7a97ab4f5bebfee7a14b9b9860
Franco Sirena <fsirena@gmail.com>
2017-10-04 10:23:12 -0300

    creating hml environment and adding baseUrl to the request service

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,13 +1,15 @@
+import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class RequestService {
-
+  baseUrl: string = environment.baseUrl;
   constructor(private http: Http) { }
 
   getData() {
+    // baseUrl = base url from services by enviroment
     return this.http.get('data/mock.json').map(res => res.json());
   }
 
